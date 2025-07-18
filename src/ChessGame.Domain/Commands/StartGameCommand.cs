028d87b5066967f2acf90a8cd9188d0abae9c6f2
guidohoogslag <Guido.Hoogslag@Navara.nl>
2025-07-18 15:48:12 +0200

    fix

--- src/ChessGame.Domain/Commands/StartGameCommand.cs
+++ src/ChessGame.Domain/Commands/StartGameCommand.cs
@@ -4,9 +4,8 @@ namespace DDD.Core;
 
 public class StartGameCommand(string player1Name, string player2Name, Guid player1Id, Guid player2Id) : IDomainCommand
 {
-    private string Player1Name { get; } = player1Name;
-    private Guid Player1Id { get; } = player1Id;
-    private string Player2Name { get; } = player2Name;
-    private Guid Player2Id { get; } = player2Id;
-
+    public string Player1Name { get; } = player1Name;
+    public Guid Player1Id { get; } = player1Id;
+    public string Player2Name { get; } = player2Name;
+    public Guid Player2Id { get; } = player2Id;
 }
