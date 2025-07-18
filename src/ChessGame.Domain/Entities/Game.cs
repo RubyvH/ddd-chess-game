0ef8291ddeb92f8338a0e18e7a198acc81904678
guidohoogslag <Guido.Hoogslag@Navara.nl>
2025-07-18 15:50:20 +0200

    update: allowed game starting

--- src/ChessGame.Domain/Entities/Game.cs
+++ src/ChessGame.Domain/Entities/Game.cs
@@ -5,21 +5,29 @@ using DDD.Core;
 
 namespace ChessGame.Domain;
 
-public class Game(Guid id) : AggregateRoot<Guid>(id)
+public class Game : AggregateRoot<Guid>
 {
+    public Game(StartGameCommand command) : base(Guid.NewGuid())
+    {
+        var rand = new Random();
+        var player1IsWhite = rand.Next(0, 1) > 0;
+        Player1 = new Player(command.Player1Name, command.Player1Id,
+            player1IsWhite ? Piece.PieceColor.White : Piece.PieceColor.Black);
+        Player2 = new Player(command.Player2Name, command.Player2Id,
+            player1IsWhite ? Piece.PieceColor.Black : Piece.PieceColor.White);
+
+        GameState =
+        [
+            new Board()
+        ];
+    }
+
     private Player Player1 { get; set; }
     private Player Player2 { get; set; }
     private List<Board> GameState { get; set; }
 
     public Task StartGame(string player1Name, Guid player1Id, string player2Name, Guid player2Id)
     {
-        var rand = new Random();
-        var player1IsWhite = rand.Next(0, 1) > 0;
-        Player1 = new Player(player1Name, player1IsWhite ? Piece.PieceColor.White : Piece.PieceColor.Black);
-        Player2 = new Player(player2Name, player1IsWhite ? Piece.PieceColor.Black : Piece.PieceColor.White);
-
-        GameState.Add(new Board());
-
         return Task.CompletedTask;
     }
 
--- src/ChessGame.Domain/ValueObjects/Player.cs
+++ src/ChessGame.Domain/ValueObjects/Player.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 
 namespace ChessGame.Domain;
 
-public class Player(string name, Piece.PieceColor color) : ValueObject
+public class Player(string name, Guid id, Piece.PieceColor color) : ValueObject
 {
     public string Name { get; } = name;
+    public Guid Id { get; } = id;
     public Piece.PieceColor Color { get; } = color;
 
     protected override IEnumerable<object> GetAtomicValues()
