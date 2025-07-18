fd3e46d29d8371a28742847d0f7fba7ccd929b09
guidohoogslag <Guido.Hoogslag@Navara.nl>
2025-07-18 16:05:20 +0200

    fix: removed recursion in equality check

--- src/ChessGame.Application/Program.cs
+++ src/ChessGame.Application/Program.cs
@@ -1,6 +1,12 @@
+using System;
+using ChessGame.Domain;
+using DDD.Core;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
+        var startCommand = new StartGameCommand("Henk", "Ook Henk", Guid.NewGuid(), Guid.NewGuid());
+        var theGame = new Game(startCommand);
     }
 }
