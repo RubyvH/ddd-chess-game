cf52bfa0fd6c6afe65a174a250b3b07b67ca6cdf
guidohoogslag <Guido.Hoogslag@Navara.nl>
2025-07-23 15:05:56 +0200

    update: repo is now singleton

--- src/ChessGame.Application/Program.cs
+++ src/ChessGame.Application/Program.cs
@@ -11,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<IGameRepository, FakeRepository>();
+builder.Services.AddSingleton<IGameRepository, FakeRepository>();
 
 var app = builder.Build();
 
@@ -25,7 +25,7 @@ app.MapGet("/{gameId}/{playerId}", async (Guid gameId, Guid playerId, IGameRepos
 {
     var commandHandler = new ShowBoardCommandHandler(repository);
     var boardState = await commandHandler.Handle(new ShowBoardCommand(gameId, playerId));
-  return boardState;
+    return boardState;
 });
 
 app.MapPost("/NewGame", async (StartGameRequest request, IGameRepository repository) =>
